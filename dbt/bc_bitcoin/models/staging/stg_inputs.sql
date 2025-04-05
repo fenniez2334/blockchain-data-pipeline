70adf5005485ffcb374506db808eec9c13e1de26
Feifei Zhao <zfeifei@alumni.upenn.edu>
2025-04-05 21:59:47 +0000

    add stage for inputs and outputs

---
+++ dbt/bc_bitcoin/models/staging/stg_inputs.sql
@@ -0,0 +1,30 @@
+{{ config(materialized='view') }}
+
+with source as (
+    select * 
+    from {{ source('staging', 'inputs') }}
+    where `hash` is not null
+),
+
+renamed as (
+    select
+        `hash` as transaction_hash,
+        index,
+        spent_transaction_hash,
+        spent_output_index,
+        script_asm,
+        script_hex,
+        sequence,
+        required_signatures,
+        type,
+        addresses,
+        value
+    from source
+)
+
+select * 
+from renamed
+
+{% if var('is_test_run', default=true) %}
+limit 30
+{% endif %}
